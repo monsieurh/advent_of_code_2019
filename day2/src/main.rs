4e82360c99fce5a017b3e36c5f45de9ea9fba2ca
ahub <ahub@riseup.net>
2019-12-02 12:29:48 +0100

    Refactoring program for easier day2 integration

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,24 +5,25 @@ use std::io::{Read, Result};
 type OpCode = usize;
 
 fn execute(opcodes: &mut Vec<OpCode>) -> Vec<OpCode> {
-    let mut i = 0;
-    while i < opcodes.len() {
-        match opcodes[i] {
+    let mut instruction_ptr = 0;
+    while instruction_ptr < opcodes.len() {
+        match opcodes[instruction_ptr] {
             1 => {
-                let target_idx = opcodes[i + 3];
-                let left_idx = opcodes[i + 1];
-                let right_idx = opcodes[i + 2];
+                let target_idx = opcodes[instruction_ptr + 3];
+                let left_idx = opcodes[instruction_ptr + 1];
+                let right_idx = opcodes[instruction_ptr + 2];
                 opcodes[target_idx] = opcodes[left_idx] + opcodes[right_idx];
-                i += 4;
+                instruction_ptr += 4;
             }
             2 => {
-                let target_idx = opcodes[i + 3];
-                let left_idx = opcodes[i + 1];
-                let right_idx = opcodes[i + 2];
+                let target_idx = opcodes[instruction_ptr + 3];
+                let left_idx = opcodes[instruction_ptr + 1];
+                let right_idx = opcodes[instruction_ptr + 2];
                 opcodes[target_idx] = opcodes[left_idx] * opcodes[right_idx];
-                i += 4;
+                instruction_ptr += 4;
             }
             99 => {
+                instruction_ptr += 1;
                 break;
             }
             _ => panic!("Invalid opcode, first value is {}", opcodes[0]),
@@ -31,34 +32,34 @@ fn execute(opcodes: &mut Vec<OpCode>) -> Vec<OpCode> {
     opcodes.clone()
 }
 
-fn main() -> Result<()> {
-    let file_path = args().nth(1).expect("Missing argument");
-    println!("Reading from file {}", file_path);
+fn run_opcode_program(memory: &mut Vec<OpCode>, noun: OpCode, verb: OpCode) -> usize {
+    println!("Starting program execution [noun:{}, verb:{}]", noun, verb);
+    memory[1] = noun;
+    memory[2] = verb;
 
-    let output = execute(&mut vec![1, 0, 0, 0, 99]);
-    assert_eq!(vec![2, 0, 0, 0, 99], output);
+    let result = execute(memory);
+    result[0]
+}
+
+fn main() -> Result<()> {
+    println!("Booting flight computer...");
+    let file_path = args().nth(1).expect("Missing argument");
+
+    println!("Reading memory file '{}'", file_path);
     let mut file = File::open(file_path)?;
     let mut buffer = String::new();
     file.read_to_string(&mut buffer)?;
-
-    // println!("{}", buffer);
     buffer = buffer.trim().to_owned();
-    // for i in buffer.split(",") {
-    //     println!("{}({})", i, i.len());
-    //     println!("parsed :{:?}", i.parse::<OpCode>().unwrap());
-    // }
-    let mut program: Vec<OpCode> = buffer
+
+    let memory: Vec<OpCode> = buffer
         .split(",")
         .map(|n| n.parse::<OpCode>().unwrap())
         .collect();
-    println!("Configuring program for crash reproduction...");
-    program[1] = 12;
-    program[2] = 2;
+    println!("Memory file loaded : {} opcodes", memory.len());
 
-    println!("Starting execution...");
-    let result = execute(&mut program);
-    println!("{}", result[0]);
+    let result = run_opcode_program(&mut memory.clone(), 12, 2);
 
+    println!("Program output (memory @0x0000):{}", result);
     Ok(())
 }
 
